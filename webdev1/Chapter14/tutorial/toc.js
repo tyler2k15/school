d0220b40064db24432c78f237d4b5924a5a36347
Tyler <tylerronek@gmail.com>
2016-04-05 01:47:18 -0500

    Section 14.1 Create the following functions: window.onload = makeTOC(); Create makeTOC(), createList(), Apply indexOf() function Create list item for each function
    Notes: Followed tutorial and double checked all steps, but it does not seem to work on chrome or firefox from my `localhost`

--- webdev1/Chapter14/tutorial/toc.js
+++ webdev1/Chapter14/tutorial/toc.js
@@ -35,3 +35,52 @@
 
 */
 
+// Generate a table of contents based on h1 through h6
+// headings when the page is loaded by the browser
+window.onload = makeTOC();
+
+//Function to generate the TOC as a nested list
+function makeTOC() {
+
+    // Source document on which the TOC is based
+    var source = document.getElementById("doc");
+
+    // Location of the table of contents box
+    var TOC = document.getElementById("toc");
+
+    // Entries in the source document on which the
+    // table of contents is based
+    var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
+
+    // Write a heading in the TOC Box
+    TOC.innerHTML = "<h1>Table of Contents</h1>";
+
+    // Create an ordered list element
+    var TOCList = document.createElement("ol");
+
+    // Append the list to the TOC
+    TOC.appendChild(TOCList);
+
+    // Create the list of table of contents entries
+    createList(source, TOCList, headings);
+}
+
+function createList(source, TOCList, headings) {
+    //
+
+    // Loop through all of the child nodes of object,
+    // sibling by sibling until no child nodes are left
+    for (var n = source.firstChild; n != null; n = n.nextSibling) {
+
+        // Determine the heading level (if any) of the current node
+        var nodeLevel = headings.indexOf(n.nodeName);
+
+        // If the node comes from a heading element,
+        // create a list item and append it to the TOC
+        if (nodeLevel != -1) {
+            var listItem = document.createElement("li");
+            listItem.innerHTML = n.innerHTML;
+            TOCList.appendChild(listItem);
+        }
+    }
+}
