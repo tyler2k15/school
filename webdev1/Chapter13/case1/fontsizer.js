428d9855b2704b424218494374f93b415c75afa5
Tyler <tylerronek@gmail.com>
2016-04-05 01:09:17 -0500

    Finalize startup() function. final step to case 1. - Chapter 13

--- webdev1/Chapter13/case1/fontsizer.js
+++ webdev1/Chapter13/case1/fontsizer.js
@@ -18,7 +18,37 @@
       based on the value of the font image button being clicked.
 	
 */
+
+/*
+        Create the startup() function
+        The purpose of this function is to apply onclick event handlers  to the
+        two font resize buttons.
+        Create the fontButtons variable containing all of the page elements that
+        belong to the fontsizer class. Loop through that collection and add an
+        event handler to each item in the collection to run the resizeText()
+        function when clicked.
+ */
+
 window.onload = startup();
 function startup() {
-    
-}
+    var fontButtons = document.getElementsByClassName("fontsizer");
+    var i;
+    alert(fontButtons.length);
+    for (i = 0; i < fontButtons.length; i++) {
+        fontButtons[i].onclick=function(){resizeText(this);};
+    }
+
+    function resizeText(objButton) {
+        var fontChange;
+        fontChange = parseFloat(objButton.value);
+
+        if (document.body.style.fontSize == "") {
+            document.body.style.fontSize = "1.0em";
+        }
+
+        var currentFontSize;
+        alert("changed");
+        currentFontSize = parseFloat(document.body.style.fontSize);
+        currentFontSize = currentFontSize+fontChange;
+        document.body.style.fontSize = "currentFontSize+em"
+    }
